6e8a09c034f241ce666433c0e39568a8b3b2c39e
weizhonzhen <czdswzz@126.com>
2020-07-23 09:33:52 +0800

    Add files via upload

---
+++ Fast.Pdf/Footer.cs
@@ -0,0 +1,17 @@
+﻿using static Fast.Pdf.PdfEnum;
+
+namespace Fast.Pdf
+{
+    public class Footer
+    {
+        public int FontSize { get; set; }
+
+        public string Content { get; set; }
+
+        public string Url { get; set; }
+
+        public Type Type { get; set; }
+
+        public Align Align { get; set; }
+    }
+}
