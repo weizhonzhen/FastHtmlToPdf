bae4b9de590f878dee3ab127baa6bb6a0d897f4e
weizhonzhen <czdswzz@126.com>
2020-10-07 21:37:46 +0800

    Add files via upload

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FastHtmlToPdf;
+using FastHtmlToPdf.Model;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var pdf = HtmlToPdf.Instance())
+            {
+                var path = string.Format("{0}\\print.htm", System.AppDomain.CurrentDomain.BaseDirectory);
+                var html = System.IO.File.ReadAllText(path);
+
+                var doc = new PdfDocument();
+                doc.DisplayFooter = true;
+                doc.Footer.Url = "http://" + HttpContext.Request.Host.Value + "/home/footer";
+                doc.Footer.Spacing = 10;
+
+                doc.DisplayHeader = true;
+                doc.Header.Url = "http://" + HttpContext.Request.Host.Value + "/home/header";
+                doc.Header.Spacing = 10;
+
+                doc.MarginBottom = 50;
+                doc.MarginTop = 50;
+
+                return File(pdf.Convert(doc, html), "application/pdf");
+            }
+        }
+
+        public IActionResult Footer()
+        {
+            return View();
+        }
+
+        public IActionResult Header()
+        {
+            return View();
+        }
+    }
+}
