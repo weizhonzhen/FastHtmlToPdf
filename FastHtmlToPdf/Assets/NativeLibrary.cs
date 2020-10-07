a59af721c968eff4906cfc3c828408a3afc0c43b
weizhonzhen <czdswzz@126.com>
2020-10-07 21:27:14 +0800

    Add files via upload

---
+++ FastHtmlToPdf/Assets/NativeLibrary.cs
@@ -0,0 +1,42 @@
+﻿using FastHtmlToPdf.Interop;
+using System;
+
+namespace FastHtmlToPdf.Assets
+{
+    internal class NativeLibrary
+    {
+        private readonly Lazy<ZipFile> LibFile;
+        private IntPtr LibHandle;
+
+        private NativeLibrary(string libraryFilename, Func<byte[]> libraryContentProvider)
+        {
+            LibFile = new Lazy<ZipFile>(() => ZipFile.Instance(libraryFilename, libraryContentProvider));
+        }
+
+        public static NativeLibrary Load(string libraryFilename, Func<byte[]> libraryContentProvider)
+        {
+            var lib = new NativeLibrary(libraryFilename, libraryContentProvider);
+            lib.LoadLibrary();
+            return lib;
+        }
+
+        public void Dispose()
+        {
+            if (LibHandle != IntPtr.Zero)
+                FreeLibrary();
+        }
+
+        private void FreeLibrary()
+        {
+            Kernel32.FreeLibrary(LibHandle);
+            LibHandle = IntPtr.Zero;
+        }
+
+        private void LoadLibrary()
+        {
+            LibHandle = Kernel32.LoadLibrary(LibFile.Value.FullPath);
+            if (LibHandle == IntPtr.Zero)
+                throw new Exception(string.Format("FastHtmlToPdf Failed to load {0}", LibFile.Value.FullPath));
+        }
+    }
+}
---
+++ FastHtmlToPdf/Assets/ZipFile.cs
@@ -0,0 +1,73 @@
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace FastHtmlToPdf.Assets
+{
+    internal class ZipFile
+    {
+        private readonly string Filename;
+        private readonly Func<byte[]> FileContent;
+
+        private ZipFile(string _Filename, Func<byte[]> _FileContent)
+        {
+            Filename = _Filename;
+            FileContent = _FileContent;
+        }
+
+        public static ZipFile Instance(string Filename, Func<byte[]> FileContent)
+        {
+            var zipFile = new ZipFile(Filename, FileContent);
+            zipFile.Create();
+            return zipFile;
+        }
+
+        private void Create()
+        {
+            if (!File.Exists(FullPath))
+                Create(FileContent());
+        }
+
+        private void Create(byte[] fileContent)
+        {
+            try
+            {
+                if (!Directory.Exists(FilesDirectory))
+                    Directory.CreateDirectory(FilesDirectory);
+
+                using (var file = File.Open(FullPath, FileMode.Create))
+                {
+                    file.Write(fileContent, 0, fileContent.Length);
+                }
+            }
+            catch (IOException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public string FullPath 
+        {
+            get
+            {
+                return Path.Combine(FilesDirectory, Filename);
+            }
+        }
+
+        private string FilesDirectory
+        {
+            get
+            {
+                return Path.Combine(Path.GetTempPath(), "FastHtmlToPdf", Version);
+            }
+        }
+
+        private string Version
+        {
+            get
+            {
+                return string.Format("{0}_{1}", Assembly.GetExecutingAssembly().GetName().Version, Environment.Is64BitProcess ? 64 : 32);
+            }
+        }
+    }
+}
