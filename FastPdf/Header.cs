834403a7a2c747c598e3224daa65f825d7dedfbe
weizhonzhen <czdswzz@126.com>
2020-10-08 14:07:35 +0800

    Add files via upload

---
+++ FastPdf/Header.cs
@@ -0,0 +1,21 @@
+﻿using static Fast.Pdf.PdfEnum;
+
+namespace Fast.Pdf
+{
+    public class Header
+    {
+        public int FontSize { get; set; }
+
+        public string Content { get; set; }
+
+        public string Url { get; set; }
+
+        public Type Type { get; set; }
+
+        public Align Align { get; set; }
+
+        public int Spacing { get; set; }
+
+        public bool Line { get; set; }
+    }
+}
