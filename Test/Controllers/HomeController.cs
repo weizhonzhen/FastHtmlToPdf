0025552dd28fcb378404a2fb0c7f83650d5e44b2
weizhonzhen <czdswzz@126.com>
2020-10-11 21:15:54 +0800

    Add files via upload

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -8,24 +8,51 @@ namespace Test.Controllers
     {
         public IActionResult Index()
         {
-            using (var pdf = HtmlToPdf.Instance())
+            using (var pdf = new HtmlToPdf())
             {
                 var path = string.Format("{0}\\print.htm", System.AppDomain.CurrentDomain.BaseDirectory);
                 var html = System.IO.File.ReadAllText(path);
 
                 var doc = new PdfDocument();
                 doc.DisplayFooter = true;
-                doc.Footer.Url = "http://" + HttpContext.Request.Host.Value + "/home/footer";
+               // doc.Footer.Url = "http://" + HttpContext.Request.Host.Value + "/home/footer";
+                doc.Footer.Spacing = 10;
+                doc.Footer.Center = "sdafsafsafsa";
+
+                doc.DisplayHeader = true;
+               // doc.Header.Url = "http://" + HttpContext.Request.Host.Value + "/home/header";
+                doc.Header.Spacing = 10;
+                doc.Header.Center = "dsafsaf";
+
+                doc.MarginBottom = 50;
+                doc.MarginTop = 50;
+
+                var bytes = pdf.Convert(doc, html);
+                return File(bytes, "application/pdf");
+            }
+        }
+
+        public IActionResult Test()
+        {
+            using (var pdf = new HtmlToPdf())
+            {
+                var path = string.Format("{0}\\print.htm", System.AppDomain.CurrentDomain.BaseDirectory);
+                var html = System.IO.File.ReadAllText(path);
+
+                var doc = new PdfDocument();
+                doc.DisplayFooter = true;
+                //doc.Footer.Url = "http://" + HttpContext.Request.Host.Value + "/home/footer";
                 doc.Footer.Spacing = 10;
 
                 doc.DisplayHeader = true;
-                doc.Header.Url = "http://" + HttpContext.Request.Host.Value + "/home/header";
+               // doc.Header.Url = "http://" + HttpContext.Request.Host.Value + "/home/header";
                 doc.Header.Spacing = 10;
 
                 doc.MarginBottom = 50;
                 doc.MarginTop = 50;
 
-                return File(pdf.Convert(doc, html), "application/pdf");
+                var bytes = pdf.Convert(doc, html);
+                return File(bytes, "application/pdf");
             }
         }
 
