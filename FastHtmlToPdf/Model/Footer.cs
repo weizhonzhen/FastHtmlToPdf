777bc138fa1f5d0106a22e14c1171b5e955193f1
weizhonzhen <czdswzz@126.com>
2020-10-11 21:10:29 +0800

    Add files via upload

--- FastHtmlToPdf/Model/Footer.cs
+++ FastHtmlToPdf/Model/Footer.cs
@@ -4,8 +4,18 @@
     {
         public int FontSize { get; set; }
 
-        public string Url { get; set; }
+        //public string Url { get; set; }
 
         public int Spacing { get; set; }
+
+        public string Center { get; set; }
+
+        public string Left { get; set; }
+
+        public string Right { get; set; }
+
+        public bool Line { get; set; }
+
+        public string FontName { get; set; }
     }
 }
