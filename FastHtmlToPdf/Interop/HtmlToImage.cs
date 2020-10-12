9cc8ae90ad4420d16123628a9fdfc33f40becb46
weizhonzhen <czdswzz@126.com>
2020-10-12 17:36:12 +0800

    Add files via upload

---
+++ FastHtmlToPdf/Interop/HtmlToImage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FastHtmlToPdf.Interop
+{
+    internal class HtmlToImage
+    {
+        #region create
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int wkhtmltoimage_init(int useGraphics);
+
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr wkhtmltoimage_create_global_settings();
+
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int wkhtmltoimage_convert(IntPtr converter);
+        #endregion
+
+        #region object and global settings
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltoimage_set_error_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] StringCallback callback);
+
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int wkhtmltoimage_set_global_setting(IntPtr settings, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))] string name, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))] string value);
+        #endregion
+
+        #region Convert
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr wkhtmltoimage_create_converter(IntPtr globalSettings, byte[] data);
+
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int wkhtmltoimage_get_output(IntPtr converter, out IntPtr data);
+
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltoimage_set_finished_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)] IntCallback callback);
+        #endregion
+
+        #region Dispose
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkhtmltoimage_destroy_converter(IntPtr converter);
+
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int wkhtmltoimage_deinit();
+        #endregion
+    }
+}
