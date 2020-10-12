2319f131946cce4ee0fc5a7c005ee5d72d0f00cd
weizhonzhen <czdswzz@126.com>
2020-10-12 17:36:39 +0800

    Add files via upload

---
+++ FastHtmlToPdf/Model/ImageDocument.cs
@@ -0,0 +1,26 @@
+﻿using FastHtmlToPdf.Context;
+
+namespace FastHtmlToPdf.Model
+{
+    public class ImageDocument
+    {
+        public int MarginTop { get; set; }
+
+        public int MarginLeft { get; set; }
+
+        public int Height { get; set; } 
+
+        public int Width { get; set; }
+
+        public int Quality { get; set; } = 94;
+
+        //public string Url { get; set; }
+
+        public FormatEnum Format { get; set; } = FormatEnum.jpg;
+
+        public int ScreenWidth { get; set; } = 800;
+
+        public bool smartWidth { get; set; }
+
+    }
+}
