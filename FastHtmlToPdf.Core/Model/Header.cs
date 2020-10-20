bc0480523dd3e990ad21d8733b6b70416a0143b6
weizhonzhen <czdswzz@126.com>
2020-10-20 14:24:50 +0800

    Add files via upload

---
+++ FastHtmlToPdf.Core/Model/Header.cs
@@ -0,0 +1,21 @@
+﻿namespace FastHtmlToPdf.Core.Model
+{
+    public class Header
+    {
+        public int FontSize { get; set; }
+
+       //public string Url { get; set; }
+
+        public int Spacing { get; set; }
+
+        public string Center { get; set; }
+
+        public string Left { get; set; }
+
+        public string Right { get; set; }
+
+        public bool Line { get; set; }
+
+        public string FontName { get; set; }
+    }
+}
---
+++ FastHtmlToPdf.Core/Model/ImageDocument.cs
@@ -0,0 +1,25 @@
+﻿using FastHtmlToPdf.Core.Context;
+
+namespace FastHtmlToPdf.Core.Model
+{
+    public class ImageDocument
+    {
+        //public int MarginTop { get; set; }
+
+        //public int MarginLeft { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public int Quality { get; set; } = 94;
+
+        //public string Url { get; set; }
+
+        public FormatEnum Format { get; set; } = FormatEnum.jpg;
+
+        public bool SmartWidth { get; set; }
+
+        public bool Transparent { get; set; }
+    }
+}
