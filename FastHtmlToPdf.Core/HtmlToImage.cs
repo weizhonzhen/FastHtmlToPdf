c66ec3f2196cc1af02f9b1ca82b22ccb0409ea39
weizhonzhen <czdswzz@126.com>
2020-10-26 11:43:28 +0800

    Add files via upload

---
+++ FastHtmlToPdf.Core/HtmlToImage.cs
@@ -0,0 +1,44 @@
+﻿using FastHtmlToPdf.Core.Model;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace FastHtmlToPdf.Core
+{
+    public class HtmlToImage : AssemblyLoadContext
+    {
+        private AssemblyLoadContext context;
+        private Assembly assembly;
+        public HtmlToImage()
+        {
+            assembly = Assembly.Load("FastHtmlToPdf.Core");
+            context = GetLoadContext(assembly);
+            context.Unloading += Context_Unloading;
+        }
+
+        private void Context_Unloading(AssemblyLoadContext obj)
+        {
+            assembly = null;
+            context = null;
+        }
+
+        public byte[] Convert(ImageDocument doc, string html)
+        {
+            foreach (var info in assembly.GetTypes().ToList())
+            {
+                if (info.FullName == "FastHtmlToPdf.Core.Context.HtmlToImage")
+                {
+                    var pdf = Activator.CreateInstance(info) as Context.HtmlToImage;
+                    return pdf.Convert(doc, html);
+                }
+            }
+            return null;
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FastHtmlToPdf.Core/HtmlToPdf.cs
@@ -0,0 +1,44 @@
+﻿using FastHtmlToPdf.Core.Model;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace FastHtmlToPdf.Core
+{
+    public class HtmlToPdf : AssemblyLoadContext
+    {
+        private AssemblyLoadContext context;
+        private Assembly assembly;
+        public HtmlToPdf()
+        {
+            assembly = Assembly.Load("FastHtmlToPdf.Core");
+            context = GetLoadContext(assembly);
+            context.Unloading += Context_Unloading;
+        }
+
+        private void Context_Unloading(AssemblyLoadContext obj)
+        {
+            assembly = null;
+            context = null;
+        }
+
+        public byte[] Convert(PdfDocument doc, string html)
+        {
+            foreach (var info in assembly.GetTypes().ToList())
+            {
+                if (info.FullName == "FastHtmlToPdf.Core.Context.HtmlToPdf")
+                {
+                    var pdf = Activator.CreateInstance(info) as Context.HtmlToPdf;
+                    return pdf.Convert(doc, html);
+                }
+            }
+            return null;
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
