35d7e3b7cbc7431d09e58e5e97ab942c07b828aa
weizhonzhen <czdswzz@126.com>
2020-12-14 16:30:48 +0800

    Add files via upload

--- FastHtmlToPdf.Core/HtmlToPdf.cs
+++ FastHtmlToPdf.Core/HtmlToPdf.cs
@@ -7,25 +7,10 @@ namespace FastHtmlToPdf.Core
 	public class HtmlToPdf
 	{
 		private static readonly HtmlToPdfQueue queue = new HtmlToPdfQueue();
-		private static Context.HtmlToPdf pdf = null;
 		private Context.HtmlToPdf converter = null;
 
 		public HtmlToPdf()
 		{
-			lock (queue)
-			{
-				if (pdf == null)
-				{
-					queue.Invoke((Action)(() => pdf = new Context.HtmlToPdf()));
-
-					AppDomain.CurrentDomain.ProcessExit += (o, e) =>
-						queue.Invoke((Action)(() => {
-							pdf.Dispose();
-							pdf = null;
-						}));
-				}
-			}
-
 			queue.Invoke((Action)(() => converter = new Context.HtmlToPdf()));
 		}
 
