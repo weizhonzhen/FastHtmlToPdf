80f2ab1020dc81ca95fccc13131e73748e410e5a
weizhonzhen <czdswzz@126.com>
2020-12-17 15:42:08 +0800

    Add files via upload

---
+++ Fast.Pdf.Core/Model/Header.cs
@@ -0,0 +1,21 @@
+﻿using static Fast.Pdf.Core.PdfEnum;
+
+namespace Fast.Pdf.Core
+{
+    public class Header
+    {
+        public int FontSize { get; set; }
+
+        public string Content { get; set; }
+
+        public string Url { get; set; }
+
+        public Type Type { get; set; }
+
+        public Align Align { get; set; }
+
+        public int Spacing { get; set; }
+
+        public bool Line { get; set; }
+    }
+}
---
+++ Fast.Pdf.Core/Model/PdfDocument.cs
@@ -0,0 +1,31 @@
+﻿using static Fast.Pdf.Core.PdfEnum;
+
+namespace Fast.Pdf.Core
+{
+    public class PdfDocument
+    {
+        public bool DisplayHeader { get; set; }
+
+        public bool DisplayFooter { get; set; }
+
+        public Header Header { get; set; } = new Header();
+
+        public Footer Footer { get; set; } = new Footer();
+
+        public int MarginTop { get; set; }
+
+        public int MarginLeft { get; set; }
+
+        public int MarginRight { get; set; }
+
+        public int MarginBottom { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public int DelayJavascript { get; set; } = 200;
+
+        public Size Size { get; set; } = PdfEnum.Size.A4;
+    }
+}
