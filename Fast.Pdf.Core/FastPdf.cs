5eecfbef01867b95fa731c748581af474ea4c0aa
weizhonzhen <czdswzz@126.com>
2020-12-17 15:44:36 +0800

    Add files via upload

---
+++ Fast.Pdf.Core/FastPdf.cs
@@ -0,0 +1,26 @@
+﻿using Fast.Pdf.Core.Threading;
+using System;
+
+namespace Fast.Pdf.Core
+{
+    public class FastPdf : IDisposable
+    {
+        private static readonly FastPdfQueue queue = new FastPdfQueue();
+        private Context.FastPdf converter = null;
+
+        public FastPdf()
+        {
+            queue.Invoke((Action)(() => converter = new Context.FastPdf()));
+        }
+
+        public byte[] Convert(PdfDocument doc, string inputHtml)
+        {
+            return (byte[])queue.Invoke((Func<string, byte[]>)((x) => converter.Convert(x, doc)), inputHtml);
+        }
+
+        public void Dispose()
+        {
+            converter.Dispose();
+        }
+    }
+}
