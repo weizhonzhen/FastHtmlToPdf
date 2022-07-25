16c0868d501d8aa7b72bc5890940e63b0b67827c
weizhonzhen <czdswzz@126.com>
2022-07-25 15:19:38 +0800

    Add files via upload

---
+++ FastHtmlToPdf.Core/FastHtmlToPdfExtension.cs
@@ -0,0 +1,22 @@
+﻿using FastHtmlToPdf.Core.Assets;
+using FastHtmlToPdf.Core.Repository;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FastHtmlToPdfExtension
+    {
+        public static IServiceCollection AddFastHtmlToPdf(this IServiceCollection serviceCollection)
+        {
+            new ZipFile();
+            serviceCollection.AddScoped<IHtmlToPdf, HtmlToPdf>();
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddFastHtmlToImage(this IServiceCollection serviceCollection)
+        {
+            new ZipFile();
+            serviceCollection.AddScoped<IHtmlToImage, HtmlToImage>();
+            return serviceCollection;
+        }
+    }
+}
